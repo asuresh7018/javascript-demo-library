8dd6e760eea8702b9fb8477fdc7d51a3383d74bc
Arjun Suresh <asuresh7018@gmail.com>
2026-05-06 09:26:32 +0000

    Allow books to be added to library
    - Book displays author, title, pages, whether read
    - Submits a form to allow books to be added

--- code.js
+++ code.js
@@ -1,20 +1,48 @@
-const myLibrary = [];
+const myLibrary = [new Book("TestTitle", "Author", 300, true)];
 
-function Book() {
+function Book(title, author, pageCount, hasBeenRead) {
     this.id = crypto.randomUUID();
-
+    this.pageCount = pageCount;
+    this.author = author;
+    this.title = title;
+    this.hasBeenRead = hasBeenRead;
+    this.readBook = function() {
+        this.hasBeenRead = true;
+    }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(formData) {
+    myLibrary.push(new Book(formData.get("title"), formData.get("author"), formData.get("pageCount"), formData.get("hasBeenRead")));
+    displayLibrary();
+}
 
+function clearLibrary () {
+    const libraryDiv = document.querySelector("#library");
+    for (book of myLibrary) {
+        bookDiv = document.querySelector("[id=\"" + book.id + "\"]");
+        if (bookDiv !== null) {
+            libraryDiv.removeChild(bookDiv);
+        }
+    }
 }
 
 function displayLibrary() {
+    clearLibrary();
     const libraryDiv = document.querySelector("#library");
     for (book of myLibrary) {
         const bookDiv = document.createElement("div");
         bookDiv.className = "book";
         bookDiv.id = book.id;
+        bookDiv.innerHTML = `
+        <div class=\"title\">${book.title}</div>
+        <div class=\"author\">${book.author}</div>
+        <div class=\"pageCount\">${book.pageCount} pages</div>`
+        if (book.hasBeenRead) {
+            bookDiv.innerHTML += `<div class=\"beenRead\">Read</div>`
+        }
+        else {
+            bookDiv.innerHTML += `<div class=\"beenRead\">Not read yet</div>`
+        }
         libraryDiv.appendChild(bookDiv);
     }
 }
@@ -24,4 +52,12 @@ const showButton = document.querySelector("#addBook");
 
 showButton.addEventListener("click", () => {
     dialog.showModal();
+})
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    dialog.close();
+    addBookToLibrary(new FormData(e.target));
 })
